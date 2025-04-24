c88ae89af537bf92bde957fa12549756b97b0bb8
skyun12 <84189491+skyun12@users.noreply.github.com>
2025-04-24 15:17:49 +0900

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '60px' }}>
+      <h1>Welcome to Vitamin Pet Landing</h1>
+      <p>건강기능식품과 반려동물 영양제를 소개하는 스마트한 쇼핑 랜딩페이지입니다.</p>
+      <img src="/images/pet-banner.jpg" alt="펫 배너" width="400" style={{ marginTop: '20px' }} />
+    </div>
+  );
+}
